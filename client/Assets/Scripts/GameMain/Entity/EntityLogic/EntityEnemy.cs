61de699fa9be1509309a7a1cd715dfc39ae60fc4
coldheart <thecoldheart@foxmail.com>
2023-10-01 00:58:28 +0800

    ✨ feat(Enemy): 怪物一定距离外自动消失

--- client/Assets/Scripts/GameMain/Entity/EntityLogic/EntityEnemy.cs
+++ client/Assets/Scripts/GameMain/Entity/EntityLogic/EntityEnemy.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 using UnityGameFramework.Runtime;
 using Random = UnityEngine.Random;
 using GameUtils;
+
 namespace p1
 {
     public class EntityEnemy : EntityLogic
@@ -12,6 +13,7 @@ namespace p1
         private EntityDataEnemy _entityDataEnemy = null;
         private Entity entityTarget;
         private GameTimer _attackTimer = new GameTimer(5, false);
+
         protected override void OnInit(object userData)
         {
             base.OnInit(userData);
@@ -20,17 +22,18 @@ namespace p1
         protected override void OnShow(object userData)
         {
             base.OnShow(userData);
-            
+
             EntityComponent entityComponent = GameEntry.GetComponent<EntityComponent>();
             entityTarget = entityComponent.GetEntity(GameConst.EntityPath[EnumEntity.Player]);
             _entityDataEnemy = userData as EntityDataEnemy;
-            transform.position = _entityDataEnemy.Position + new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0f);
-
-
+            transform.position = _entityDataEnemy.Position +
+                                 new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0f);
         }
 
         protected override void OnUpdate(float elapseSeconds, float realElapseSeconds)
         {
+            CheckDistanceAndDestroy();
+            
             _attackTimer.UpdateAsFinish(elapseSeconds, () =>
             {
                 _attackTimer.Reset();
@@ -38,6 +41,7 @@ namespace p1
             });
             MoveControl(elapseSeconds);
         }
+
         private void MoveControl(float elapseSeconds)
         {
             // 自动追踪目标
@@ -48,7 +52,15 @@ namespace p1
                 moveInput.Normalize();
                 transform.position += moveInput * (_entityDataEnemy.MoveSpeed * elapseSeconds);
             }
-            
+        }
+
+        private void CheckDistanceAndDestroy()
+        {
+            if (Vector3.Distance(transform.position, entityTarget.transform.position) >
+                EnemySpawner.Instance.MaxDistance)
+            {
+                GameEntry.GetComponent<EntityComponent>().HideEntity(this.Entity.Id);
+            }
         }
 
         private void OnCollisionEnter2D(Collision2D other)
@@ -59,5 +71,5 @@ namespace p1
                 _attackTimer.CanRun = true;
             }
         }
-    } 
+    }
 }
--- client/Assets/Scripts/GameMain/Helper/EnemySpawner.cs
+++ client/Assets/Scripts/GameMain/Helper/EnemySpawner.cs
@@ -7,17 +7,28 @@ namespace p1
     public class EnemySpawner : GlobalSingleton<EnemySpawner>
     {
         public int MaxEnemyCount = 100;
-        private int _curEnemyCount = 0;
+
+        private int _curEnemyCount => GameEntry.GetComponent<EntityComponent>()
+            .GetEntityGroup(GameConst.EntityGroup[EnumEntityGroup.Enemy]).EntityCount;
+
         private GameTimer _enemySpawnTimer = new GameTimer(0.5f);
         private Transform _enemySpawner;
         private Transform _minSpawnPoint;
         private Transform _maxSpawnPoint;
+        private float _maxDistance;
+
+        public float MaxDistance
+        {
+            get => _maxDistance;
+            private set => _maxDistance = value;
+        }
 
         public void Init()
         {
             _enemySpawner = transform;
             _minSpawnPoint = _enemySpawner.Find("MinSpawnPoint");
             _maxSpawnPoint = _enemySpawner.Find("MaxSpawnPoint");
+            _maxDistance = Vector3.Distance(_maxSpawnPoint.position, Vector3.zero) + 4f;
         }
 
         public void UpdateSpawner(float delta)
@@ -29,7 +40,6 @@ namespace p1
 
         private void EnemySpawn()
         {
-            _curEnemyCount++;
             int id = IdGenerator.Instance.GetNextID();
             var entityComponent = GameEntry.GetComponent<EntityComponent>();
             entityComponent.ShowEntity<EntityEnemy>(id,
@@ -54,6 +64,7 @@ namespace p1
                 spawnPoint.x = Random.Range(_minSpawnPoint.position.x, _maxSpawnPoint.position.x);
                 spawnPoint.y = Random.Range(0f, 1f) > 0.5f ? _minSpawnPoint.position.y : _maxSpawnPoint.position.y;
             }
+
             Debug.Log("spawn in: " + spawnPoint);
             return spawnPoint;
         }
